21287ce4779001c0c79782d54b2fb4919201a7a4
shubhambbk50 <94617569+shubhambbk50@users.noreply.github.com>
2022-10-15 18:20:31 +0530

    Time: 18 ms (44.06%), Space: 15.2 MB (43.50%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string str;
+        string str= s;  // we have done this because we can't declare 
+                        // string str[s.size()] 
+                        // but we can do so by alloting dynamic memory
+                        // in that case we have to use "\0"
         for(int i=0; i<indices.size(); i++){
-            for(int j=0; j<indices.size();j++){
-                if(indices[j]==i){
-                    str+=s[j];
-                }
-            }
+            str[indices[i]]=s[i];
         }
         return str;
     }
