4ef4f086bea428886766a764aa30a01d76cbc217
Shubham Verma <94617569+shubhambbk50@users.noreply.github.com>
2023-02-20 05:14:10 +0530

    Added solution - LeetHub

---
+++ Count Cyclic Paths - GFG/count-cyclic-paths.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int countPaths(int N){
+        // code here 
+        if(N<2)
+        return 0;
+       long int m=1000000007;
+       long int t=0;
+       int i=2;
+         while(i<=N){
+                t=(t*3)%m;
+                 if(i%2!=0)
+                    t-=3;
+                   else t=(t+3)%m;
+                  i++;
+            }
+        return t;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        
+        Solution ob;
+        cout << ob.countPaths(N) << endl;
+    }
+    return 0; 
+}
+
+// } Driver Code Ends
