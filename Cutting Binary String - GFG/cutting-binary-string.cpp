6ccc0dfc6ef7ba343024c911eb24a62b50c564d9
Shubham Verma <94617569+shubhambbk50@users.noreply.github.com>
2023-05-16 04:36:42 +0530

    Added solution - LeetHub

---
+++ Cutting Binary String - GFG/cutting-binary-string.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+     bool is(int n)
+    {
+        for(int i=0;i<16;i++)
+        {
+            if(pow(5,i)==n)
+            {
+                
+                return true;
+            }
+        }
+        return false;
+        
+    }
+    int solve(int i,int n,int power,string &s)
+    {
+        
+        if(i==0)
+        {
+           
+           if(s[i]=='1'&&is(n+pow(2,power)))
+           {
+               return 1;
+           }
+           return 1e9;
+            
+        }
+       
+        int t=solve(i-1,n+(pow(2,power)*(s[i]-'0')),power+1,s);
+        if(s[i]=='1')
+        {
+            if(is(n+pow(2,power))==true)
+            {
+             t=min(1+solve(i-1,0,0,s),t);
+            }
+        }
+        
+        return t;
+    }
+    int cuts(string s){
+        
+        int ans=solve(s.size()-1,0,0,s);
+        if(s.size()==1&&s[0]=='1')
+        {
+            return 1;
+        }
+        if(ans>=1e9)
+        {
+            return -1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+   string s;
+   while(t--)
+   {
+    cin>>s;
+    Solution obj;
+    int res=obj.cuts(s);
+    cout<<res<<endl;
+
+   }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
