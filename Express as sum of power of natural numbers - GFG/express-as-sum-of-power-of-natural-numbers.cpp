9ef1222a776643449ca76cf1415fa13ab55b0809
Shubham Verma <94617569+shubhambbk50@users.noreply.github.com>
2023-05-26 07:35:32 +0530

    Added solution - LeetHub

---
+++ Express as sum of power of natural numbers - GFG/express-as-sum-of-power-of-natural-numbers.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    int numOfWays(int n, int x)
+    {
+        // code here
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(n, x, 1, dp);
+    }
+    
+    long long solve(int n, int x, int a, vector<vector<int>>& dp) {
+        if(n == 0) return 1;
+        int value = 1;
+        for(int i = 0; i < x; i++) value *= a;
+        if(value > n) return 0;
+        if(dp[n][a] != -1) return dp[n][a];
+        int mod = 1e9 + 7;
+        return dp[n][a] = (solve(n, x, a+1, dp) + solve(n-value, x, a+1, dp)) % mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int  n, x;
+        cin >> n >> x;
+        Solution ob;
+        cout<<ob.numOfWays(n, x)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
